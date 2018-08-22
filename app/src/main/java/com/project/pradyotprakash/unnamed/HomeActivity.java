57a821e960cfffea8de28276efdc8b797fb1ce27
pradyot <pradyotprksh4@gmail.com>
2018-08-22 23:17:06 +0530

    Working On Home Page

--- app/src/main/java/com/project/pradyotprakash/unnamed/HomeActivity.java
+++ app/src/main/java/com/project/pradyotprakash/unnamed/HomeActivity.java
@@ -1,13 +1,56 @@
 package com.project.pradyotprakash.unnamed;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private BottomNavigationView mMainNav;
+    private FrameLayout mMainFrame;
+    private HomeFragment homeFragment;
+    private FollowingFragment followingFragment;
+    private AccountFragment accountFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        homeFragment = new HomeFragment();
+        followingFragment = new FollowingFragment();
+        accountFragment = new AccountFragment();
+        mMainFrame = findViewById(R.id.main_frame);
+        mMainNav = findViewById(R.id.main_nav);
+        setFragment(homeFragment);
+        mMainNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.nav_home : mMainNav.setItemBackgroundResource(R.color.colorPrimaryDark);
+                        setFragment(homeFragment);
+                        return true;
+                    case R.id.nav_follow : mMainNav.setItemBackgroundResource(R.color.colorPrimaryDarkShade1);
+                        setFragment(followingFragment);
+                        return true;
+                    case R.id.nav_account : mMainNav.setItemBackgroundResource(R.color.colorPrimaryDarkShade2);
+                        setFragment(accountFragment);
+                        return true;
+                    default :
+                        return false;
+                }
+            }
+        });
+    }
+
+    private void setFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.setCustomAnimations(R.anim.slide_in_bottom, R.anim.slide_in_top);
+        fragmentTransaction.replace(R.id.main_frame, fragment);
+        fragmentTransaction.commit();
     }
 }
