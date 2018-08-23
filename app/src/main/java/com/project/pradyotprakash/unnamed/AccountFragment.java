c356658cb3a877295cfbd188990f2d80f3f77e61
pradyot <pradyotprksh4@gmail.com>
2018-08-23 17:06:55 +0530

    Account Setting Work

--- app/src/main/java/com/project/pradyotprakash/unnamed/AccountFragment.java
+++ app/src/main/java/com/project/pradyotprakash/unnamed/AccountFragment.java
@@ -1,18 +1,34 @@
 package com.project.pradyotprakash.unnamed;
 
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
+
+import java.util.Objects;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+import static android.app.Activity.RESULT_OK;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class AccountFragment extends Fragment {
-
 
     public AccountFragment() {
         // Required empty public constructor
@@ -23,7 +39,9 @@ public class AccountFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_account, container, false);
+        View view = inflater.inflate(R.layout.fragment_account, container, false);
+
+        return view;
     }
 
 }
