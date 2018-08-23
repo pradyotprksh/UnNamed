ff5c6632f03cdf0483aa8b0a1c0728d2b9d0e9a1
pradyot <pradyotprksh4@gmail.com>
2018-08-23 18:11:46 +0530

    Account Setting Work Still

--- app/src/main/java/com/project/pradyotprakash/unnamed/AccountFragment.java
+++ app/src/main/java/com/project/pradyotprakash/unnamed/AccountFragment.java
@@ -7,12 +7,21 @@ import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.SyncStateContract;
+import android.support.annotation.RequiresApi;
+import android.support.constraint.ConstraintLayout;
+import android.support.constraint.ConstraintSet;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
+import android.text.method.ScrollingMovementMethod;
+import android.transition.TransitionManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.theartofdev.edmodo.cropper.CropImage;
@@ -34,12 +43,38 @@ public class AccountFragment extends Fragment {
         // Required empty public constructor
     }
 
+    private Boolean isOpen = false;
+    private ConstraintSet layout1, layout2;
+    private ConstraintLayout constraintLayout;
+    private ImageView imageViewPhoto;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_account, container, false);
+
+        imageViewPhoto = view.findViewById(R.id.profileImage);
+        layout1 = new ConstraintSet();
+        layout2 = new ConstraintSet();
+        constraintLayout = view.findViewById(R.id.constraint_layout);
+        layout2.clone(getContext(), R.layout.expanded_fragment_account);
+        layout1.clone(constraintLayout);
+        imageViewPhoto.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+            @Override
+            public void onClick(View v) {
+                if (!isOpen) {
+                    TransitionManager.beginDelayedTransition(constraintLayout);
+                    layout2.applyTo(constraintLayout);
+                    isOpen = !isOpen;
+                } else {
+                    TransitionManager.beginDelayedTransition(constraintLayout);
+                    layout1.applyTo(constraintLayout);
+                    isOpen = !isOpen;
+                }
+            }
+        });
 
         return view;
     }
